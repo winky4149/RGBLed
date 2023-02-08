6aa643c58c1ab0e656d58fec2028079b7b5629e7
winky4149 <digitalpuppy@kookmin.ac.kr>
2023-02-08 13:38:55 +0900

    first status

---
+++ Core/Inc/uart.h
@@ -0,0 +1,26 @@
+/*
+ * uart.h
+ *
+ *  Created on: Feb 3, 2023
+ *      Author: npedu030
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+
+
+#endif /* INC_UART_H_ */
+
+#include "main.h"
+
+int sub(int i,int j)
+{
+	for(;i<10;i++)
+  		{
+  			for(;j<10;j++)
+  			{
+  				printf("%d x %d = %d\n",i,j,i*j);
+  			}
+  		}
+}
